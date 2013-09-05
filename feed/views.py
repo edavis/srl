1553d1bebbde36c45b46d56f482cd699e87bc897
Eric Davis <ed@npri.org>
2013-09-04 22:08:17 -0700

    views.py: add view_rss function

--- feed/views.py
+++ feed/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+import re
+import requests
+from lxml import etree
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def view_rss(request):
+    rss_url = request.GET.get("url")
+    if rss_url is not None:
+        try:
+            doc = etree.parse(rss_url)
+        except IOError:
+            resp = requests.get(rss_url)
+            doc = etree.fromstring(resp.content)
+        output = etree.tostring(doc, pretty_print=True)
+        output = re.sub(r'([<?xml-stylesheet].+?\?>)', '', output)
+        content_type = request.GET["ct"]
+        return HttpResponse(output, content_type=content_type)
+    else:
+        return render(request, "feed/view_rss.html")
